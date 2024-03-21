e05d627915c98b5ed24a16bf5a466e418f1cc830
David Magalhaes <david.magalhaes@samsung.com>
2024-03-21 11:41:56 -0400

    add field validation to the edito form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,13 +66,23 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 export async function updateInvoice(
   id: string,
+  prevState: State,
   formData: FormData
 ) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if(!validatedFields.success){
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.'
+    }
+  }
+
+  const {customerId, status, amount} = validatedFields.data;
 
   const amountInCents = amount * 100;
 
